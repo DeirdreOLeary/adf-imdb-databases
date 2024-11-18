b7544f989b9627aef66d233dc39d9c96b9528df8
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2024-11-18 18:06:58 +0100

    Added silver layer stored procs to transform & load data to the silver Movies & TVSeries tables as part of the ADF pipeline.

---
+++ adf-imdb-databases/sqldb-dw-imdb-01/Schema/silver/Stored Procedures/ADF_Transform_Load_TVSeries.sql
@@ -0,0 +1,85 @@
+﻿/***********************************************************************************************
+Purpose:	This stored proc is called by the Azure Data Factory pipeline to 
+            transform data from the bronze title_basics table to the silver TVSeries table.
+
+Pipeline:	pl_trans_silver_transform_load_data (adf-imdb-to-sqldb-01)
+
+Activity:	Transform & Load TV Series data
+
+***********************************************************************************************/
+CREATE PROCEDURE [silver].[ADF_Transform_Load_TVSeries]
+AS
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        
+        /* Get the details from the bronze titles_basics table for all non-adult TV series with a primary title.
+           Transform & load the data into the silver TVSeries table. */
+		;WITH [EpisodesNulled] AS (
+			SELECT [parentTconst]
+				,CASE [seasonNumber]
+					WHEN '\N' THEN NULL
+					ELSE CAST([seasonNumber] AS SMALLINT)
+				END AS [seasonNumber]
+				,CASE [episodeNumber]
+					WHEN '\N' THEN NULL
+					ELSE CAST([episodeNumber] AS INT)
+				END AS [episodeNumber]
+			FROM [bronze].[title_episode]
+		)
+		,[SeasonsAndEpisodes] AS (
+			SELECT [parentTconst]
+				,ISNULL(MAX([seasonNumber]), 0) AS [NumberOfSeasons]
+				,COUNT([episodeNumber]) AS [NumberOfEpisodes]
+			FROM [EpisodesNulled]
+			GROUP BY [parentTconst]
+		)
+        ,[TVSeriesTitles] AS (
+            SELECT t.[tconst] AS [TitleKey],
+                t.[primaryTitle] AS [Title],
+                CASE t.[startYear]
+                    WHEN '\N' THEN NULL
+                    ELSE CAST(t.[startYear] AS CHAR(4))
+                END AS [StartYear],
+                CASE t.[endYear]
+                    WHEN '\N' THEN NULL
+                    ELSE CAST(t.[endYear] AS CHAR(4))
+                END AS [EndYear],
+                s.[NumberOfSeasons],
+                s.[NumberOfEpisodes],
+                CASE t.[genres]
+                    WHEN '\N' THEN NULL
+                    ELSE t.[genres]
+                END AS [Genres]
+			    FROM [bronze].[title_basics] t
+			    INNER JOIN [SeasonsAndEpisodes] s
+			    ON t.[tconst] = s.[parentTconst]
+			    WHERE [titleType] = 'tvSeries'
+				    AND isAdult = 0
+        )
+        INSERT INTO [silver].[TVSeries] (
+            [TitleKey],
+	        [Title],
+	        [StartYear],
+            [EndYear],
+	        [NumberOfSeasons],
+            [NumberOfEpisodes],
+	        [Genres]
+        )
+        SELECT [TitleKey],
+	        [Title],
+	        [StartYear],
+            [EndYear],
+	        [NumberOfSeasons],
+            [NumberOfEpisodes],
+	        [Genres]
+        FROM [TVSeriesTitles];
+
+    END TRY
+    BEGIN CATCH
+
+        ;THROW
+
+    END CATCH
+
+GO
---
+++ adf-imdb-databases/sqldb-dw-imdb-01/Schema/silver/Stored Procedures/ADF_Truncate_Tables.sql
@@ -0,0 +1,31 @@
+﻿/***********************************************************************************************
+Purpose:	This stored proc is called by the Azure Data Factory pipeline to 
+			truncate the silver tables.
+
+Pipeline:	pl_trans_silver_transform_load_data (adf-imdb-to-sqldb-01)
+
+Activity:	Truncate silver tables
+
+***********************************************************************************************/
+CREATE PROCEDURE [silver].[ADF_Truncate_Tables]
+AS
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+
+        /* Truncate all silver tables */
+		TRUNCATE TABLE [silver].[Actors];
+        TRUNCATE TABLE [silver].[Directors];
+		TRUNCATE TABLE [silver].[Movies];
+        TRUNCATE TABLE [silver].[Ratings];
+		TRUNCATE TABLE [silver].[TVSeries];
+        TRUNCATE TABLE [silver].[Writers];
+
+    END TRY
+    BEGIN CATCH
+
+        ;THROW
+
+    END CATCH
+
+GO
