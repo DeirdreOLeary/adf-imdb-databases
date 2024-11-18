1beeb100410941934fce39ae68bcf884c2ba747a
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2024-11-18 14:57:31 +0100

    Added bronze.ADF_Truncate_Tables to truncate all bronze tables at the start of the ETL pipeline.

---
+++ adf-imdb-databases/sqldb-dw-imdb-01/Schema/bronze/Stored Procedures/ADF_Truncate_Tables.sql
@@ -0,0 +1,31 @@
+﻿/***********************************************************************************************
+Purpose:	This stored proc is called by the Azure Data Factory pipeline to 
+			truncate the bronze tables.
+
+Pipeline:	pl_data_bronze_extract_files (adf-imdb-to-sqldb-01)
+
+Activity:	Truncate all bronze tables
+
+***********************************************************************************************/
+CREATE PROCEDURE [bronze].[ADF_Truncate_Tables]
+AS
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+
+        /* Truncate all bronze tables */
+		TRUNCATE TABLE [bronze].[name_basics];
+        TRUNCATE TABLE [bronze].[title_basics];
+        TRUNCATE TABLE [bronze].[title_crew];
+        TRUNCATE TABLE [bronze].[title_episode];
+        TRUNCATE TABLE [bronze].[title_principals];
+        TRUNCATE TABLE [bronze].[title_ratings];
+
+    END TRY
+    BEGIN CATCH
+
+        ;THROW
+
+    END CATCH
+
+GO
