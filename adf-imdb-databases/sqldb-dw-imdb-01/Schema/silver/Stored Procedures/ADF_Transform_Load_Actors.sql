0acc14c4de1cd7b3db9943e328829bc2c1f5ff05
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2024-11-25 16:34:04 +0100

    Added silver layer stored procs to transform & load data to the silver Actors table as part of the ADF pipeline.

---
+++ adf-imdb-databases/sqldb-dw-imdb-01/Schema/silver/Stored Procedures/ADF_Transform_Load_Actors.sql
@@ -0,0 +1,59 @@
+﻿/***********************************************************************************************
+Purpose:	This stored proc is called by the Azure Data Factory pipeline to 
+            transform data from the bronze name_basics & title_principals tables to the silver Actors table.
+
+Pipeline:	pl_trans_silver_transform_load_data (adf-imdb-to-sqldb-01)
+
+Activity:	Transform & Load Actors data
+
+***********************************************************************************************/
+CREATE PROCEDURE [silver].[ADF_Transform_Load_Actors]
+AS
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        
+        /* Get the details from the bronze name_basics & title_principals tables for all actors & associated characters.
+           Transform & load the data into the silver Actors table. */
+		;WITH [ActorNames] AS (
+            SELECT LEFT(nb.[nconst], 10) AS [NameKey],
+                LEFT(tp.[tconst], 10) AS [TitleKey],
+                LEFT(nb.[primaryName], 255) AS [Name],
+                CASE nb.[birthYear]
+                    WHEN '\N' THEN NULL
+                    ELSE LEFT(nb.[birthYear], 4)
+                END AS [BirthYear],
+                CASE nb.[deathYear]
+                    WHEN '\N' THEN NULL
+                    ELSE LEFT(nb.[deathYear], 4)
+                END AS [DeathYear],
+                LEFT(tp.[characters], 255) AS [Character]
+            FROM [bronze].[name_basics] nb
+            INNER JOIN [bronze].[title_principals] tp
+            ON nb.[nconst] = tp.[nconst]
+            WHERE [category] IN ('actor', 'actress')
+		)
+        INSERT INTO [silver].[Actors] (
+            [NameKey],
+	        [TitleKey],
+	        [Name],
+	        [BirthYear],
+	        [DeathYear],
+	        [Character]
+        )
+        SELECT [NameKey],
+	        [TitleKey],
+	        [Name],
+	        [BirthYear],
+	        [DeathYear],
+	        [Character]
+        FROM [ActorNames];
+
+    END TRY
+    BEGIN CATCH
+
+        ;THROW
+
+    END CATCH
+
+GO
