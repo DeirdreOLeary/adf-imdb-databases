a328d43044f8237ce86e8d8f577f569193789d88
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2024-11-29 16:39:17 +0100

    Added silver layer stored procs to transform & load data to the silver Ratings table as part of the ADF pipeline.

--- adf-imdb-databases/sqldb-dw-imdb-01/Schema/silver/Tables/Ratings.sql
+++ adf-imdb-databases/sqldb-dw-imdb-01/Schema/silver/Tables/Ratings.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [silver].[Ratings] (
 	[TitleKey]		VARCHAR(10)		NOT NULL,
 	[AverageRating]	DECIMAL(3,1)	NOT NULL,
-	[numVotes]		INT				NOT NULL,
+	[NumberOfVotes]	INT				NOT NULL,
 	CONSTRAINT [PK_silver_Ratings]  PRIMARY KEY CLUSTERED ([TitleKey])
 );
